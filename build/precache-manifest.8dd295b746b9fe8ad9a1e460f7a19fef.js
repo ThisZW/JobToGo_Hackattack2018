84f235316c4aa31e974e477b86fa1cfd28afd551
ThisZW <everbread1234@gmail.com>
2018-10-28 07:57:30 -0400

    client to root

---
+++ build/precache-manifest.8dd295b746b9fe8ad9a1e460f7a19fef.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "0881c55d12a7bcb3877f28492bd42fbe",
+    "url": "/static/media/image.0881c55d.jpg"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "b1a308fdf5d66d17dda6",
+    "url": "/static/js/main.b1a308fd.chunk.js"
+  },
+  {
+    "revision": "1dab5cd83931482a203c",
+    "url": "/static/js/1.1dab5cd8.chunk.js"
+  },
+  {
+    "revision": "b1a308fdf5d66d17dda6",
+    "url": "/static/css/main.d0c03029.chunk.css"
+  },
+  {
+    "revision": "1dab5cd83931482a203c",
+    "url": "/static/css/1.98b31319.chunk.css"
+  },
+  {
+    "revision": "aab027099b64395d51f73a2f9b329470",
+    "url": "/index.html"
+  }
+];
